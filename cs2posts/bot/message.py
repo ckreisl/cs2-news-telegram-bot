7a7324c745f8ee9e029302685fd35ee6604d9215
ckreisl <christoph.kreisl@gmx.de>
2024-06-06 18:04:58 +0200

    External news formatting

--- cs2posts/bot/message.py
+++ cs2posts/bot/message.py
@@ -193,14 +193,42 @@ class CounterStrikeExternalMessage(TelegramMessage):
         post.url = Utils.get_redirected_url(post.url)
         self.post = post
 
-        # TODO: We skip the <img> or <a> (link) tag for now
         soup = BeautifulSoup(post.contents, "html.parser")
 
-        msg = f"<b>{post.title}</b>\n"
+        for img in soup.find_all("img"):
+            img.decompose()
+
+        for br in soup.find_all("br"):
+            br.decompose()
+
+        read_more_all = soup.find_all("a")
+
+        read_more = []
+        for a in read_more_all:
+            if "read more" in a.text.lower():
+                read_more.append(a)
+            if "read the rest of the story" in a.text.lower():
+                read_more.append(a)
+
+        if len(read_more) > 0:
+            for a in read_more:
+                a.extract()
+
+        content = str(soup)
+        content = content.replace("<p>", "").replace("</p>", "")
+        content = content.replace("<br/>", "")
+        content = content.lstrip().rstrip()
+
+        msg = "🔗 <b>External News</b>\n\n"
+        msg += f"<b>{post.title}</b>\n"
         msg += f"({post.date_as_datetime})\n"
         msg += "\n"
-        msg += soup.get_text()
+        msg += content
         msg += "\n\n"
+
+        if len(read_more) > 0:
+            post.url = read_more[0].get("href")
+
         msg += f"Source: <a href='{post.url}'>Link</a>"
 
         super().__init__(msg)
@@ -211,7 +239,7 @@ class CounterStrikeExternalMessage(TelegramMessage):
                 chat_id=chat_id,
                 text=msg,
                 parse_mode=ParseMode.HTML,
-                disable_web_page_preview=False)
+                disable_web_page_preview=True)
 
 
 class TelegramMessageFactory:
