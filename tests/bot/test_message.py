e27a5b2ce17d494ab273f2373fb83db7ea97dc1b
Christoph Kreisl <christoph.kreisl@gmx.de>
2024-11-15 21:07:29 +0100

    fix missing newlines (#28)

--- tests/bot/test_message.py
+++ tests/bot/test_message.py
@@ -83,7 +83,7 @@ def test_counter_strike_update_message(mocked_cs2_update_post):
         mocked_get.return_value.url = "https://test.com"
         mocked_cs2_update_post.body = "[ UI ]\n[list]\n[*]Fixed cases where there was a visible delay loading map images in the Play menu\n[*]Fixed a bug where items that can't be equipped were visible in the Loadout menu\n[*]Fixed a bug where loadout items couldn't be unequipped\n[*]Fixed a bug where loadout changes weren't saved if the game was quit shortly after making changes\n[*]Fixed a bug where loadout changes on the main menu character were delayed\n[/list]\n[ MISC ]\n[list]\n[*]Fixed some visual issues with demo playback\n[*]Fixed an issue where animations would not play back correctly in a CSTV broadcast\n[*]Adjusted wear values of some community stickers to better match CS:GO\n[/list]\n[ MAPS ]\n[i]Ancient:[/i][list]\n[*]Added simplified grenade collisions to corner trims and central pillar on B site\n[/list]\n[i]Anubis:[/i][list]\n[*]Adjusted clipping at A site steps between Walkway and Heaven\n[/list]"
         msg = CounterStrikeUpdateMessage(post=mocked_cs2_update_post)
-        expected = "<b>Release Notes for 2/13/2009</b>\n(2009-02-13 23:31:30)\n\nmy content(Author: Valve)\n\nSource: <a href='https://test.com'>Link</a>"
+        expected = "<b>Release Notes for 2/13/2009</b>\n(2009-02-13 23:31:30)\n\nmy content\n\n(Author: Valve)\n\nSource: <a href='https://test.com'>Link</a>"
         assert len(msg.messages) == 1
         assert msg.message == expected
 
